a1a989fe15cbc1298eca9c77a02152561bedecfa
Arnav Kohli <arnavkohli@gmail.com>
2020-08-10 21:06:51 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello_world():
+	return "Hello, world!"
+
+if __name__ == '__main__':
+	app.run('0.0.0.0')
+
